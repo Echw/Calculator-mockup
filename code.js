d53329857dc9c80e7a9eb125be34b38f3add7a51
echw <echwedczuk0@gmail.com>
2020-04-26 15:51:13 +0200

    add calculator project

---
+++ code.js
@@ -0,0 +1,5 @@
+    var numbers = document.getElementsByClassName("number")
+    var display = document.getElementsByClassName('display')
+    var clear = document.getElementsByClassName('clear')
+    var one = document.getElementsByClassName('one')
+
