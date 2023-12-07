9de1fc0d72b76225efdcb201672e72524465beb1
echw <echwedczuk0@gmail.com>
2023-12-07 19:50:50 +0100

    Init project and add events listeners to number buttons

--- code.js
+++ code.js
@@ -1,5 +1,29 @@
-    var numbers = document.getElementsByClassName("number")
-    var display = document.getElementsByClassName('display')
-    var clear = document.getElementsByClassName('clear')
-    var one = document.getElementsByClassName('one')
+const calculator = {
+  numbers: Array.from(document.querySelectorAll(".number")),
+  display: document.querySelector("#display"),
+  clear: document.querySelector("#clear"),
 
+  clickedNumber: 0,
+
+  initEventListeners() {
+    this.numbers.forEach((number) => {
+      number.addEventListener("click", (e) => {
+        console.log(e.target.innerText);
+        this.clickedNumber = parseInt(e.target.innerText);
+        this.updateDisplay();
+      });
+    });
+  },
+
+  updateDisplay() {
+    this.display.value += this.clickedNumber;
+  },
+
+  init() {
+    console.log(this.numbers);
+    this.initEventListeners();
+    this.updateDisplay();
+  },
+};
+
+calculator.init();
