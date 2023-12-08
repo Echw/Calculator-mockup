90c4192c5fd2c090c32e8f9fd45fc66905086322
echw <echwedczuk0@gmail.com>
2023-12-08 12:45:29 +0100

    add addition

--- code.js
+++ code.js
@@ -1,26 +1,62 @@
 const calculator = {
-  numbers: Array.from(document.querySelectorAll(".number")),
   display: document.querySelector("#display"),
+  buttons: document.querySelector(".buttons"),
   clear: document.querySelector("#clear"),
-
-  clickedNumber: 0,
+  displayValue: 0,
+  clickedNumber: "",
+  firstValue: undefined,
+  secondValue: undefined,
+  clickedMathMark: undefined,
+  mathClicked: false,
 
   initEventListeners() {
-    this.numbers.forEach((number) => {
-      number.addEventListener("click", (e) => {
-        console.log(e.target.innerText);
-        this.clickedNumber = parseInt(e.target.innerText);
+    this.buttons.addEventListener("click", (e) => {
+      if (e.target.classList.contains("number")) {
+        this.mathClicked = false;
+        this.firstValue = parseInt(e.target.innerText);
+        this.displayValue = this.firstValue;
         this.updateDisplay();
-      });
+      } else if (e.target.innerText == "=") {
+        this.mathClicked = true;
+        this.math(this.clickedMathMark);
+      } else if (e.target.classList.contains("math")) {
+        this.mathClicked = true;
+        this.secondValue = this.firstValue;
+        this.clickedMathMark = e.target.innerText;
+        this.firstValue = this.displayValue = 0;
+        this.updateDisplay();
+      }
     });
   },
 
+  math(operation) {
+    switch (operation) {
+      case "+":
+        this.addition();
+        break;
+
+      default:
+        break;
+    }
+  },
+
   updateDisplay() {
-    this.display.value += this.clickedNumber;
+    if (!this.mathClicked) {
+      this.display.value == 0
+        ? (this.display.value = this.displayValue)
+        : (this.display.value += this.displayValue);
+    } else {
+      this.display.value = this.displayValue;
+    }
+  },
+
+  addition() {
+    this.displayValue = this.firstValue + this.secondValue;
+    this.updateDisplay();
+    console.log(this.displayValue, this.firstValue, this.secondValue);
   },
 
   init() {
-    console.log(this.numbers);
     this.initEventListeners();
     this.updateDisplay();
   },
