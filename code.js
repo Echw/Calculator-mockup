d486a7b5ab46bbcf52a85e3d96420de73f379e27
echw <echwedczuk0@gmail.com>
2023-12-12 14:19:05 +0100

    update remove functionality

--- code.js
+++ code.js
@@ -7,6 +7,7 @@ const calculator = {
   secondNumber: 0,
   clickedMathMark: undefined,
   mathClicked: false,
+  dotaAdded: false,
 
   initEventListeners() {
     this.buttons.addEventListener("click", (e) => {
@@ -24,9 +25,56 @@ const calculator = {
           this.displayValue = this.secondNumber;
         }
         this.updateDisplay();
+      } else if (e.target.classList.contains("remove")) {
+        if (this.firstNumber.slice(-1) == ".") {
+          this.dotaAdded = false;
+        }
+        if (!this.mathClicked) {
+          this.firstNumber == 0
+            ? (this.firstNumber = 0)
+            : (this.firstNumber = this.firstNumber.slice(0, -1));
+          this.displayValue = this.firstNumber;
+        } else {
+          this.secondNumber == 0
+            ? (this.secondNumber = 0)
+            : (this.secondNumber = this.secondNumber.slice(0, -1));
+          this.displayValue = this.secondNumber;
+        }
+        if (this.displayValue == "") {
+          this.displayValue = 0;
+        }
+        this.updateDisplay();
       } else if (e.target.innerText == "=") {
         this.mathClicked = false;
         this.math(this.clickedMathMark);
+        this.firstNumber = this.displayValue;
+        this.secondNumber = 0;
+      } else if (e.target.innerText == "C") {
+        this.displayValue = 0;
+        this.firstNumber = 0;
+        this.secondNumber = 0;
+        this.clickedMathMark = undefined;
+        this.mathClicked = false;
+        this.dotaAdded = false;
+        this.updateDisplay();
+      } else if (e.target.innerText == ".") {
+        if (!this.dotaAdded) {
+          if (!this.mathClicked) {
+            this.dotaAdded = true;
+            this.firstNumber == 0
+              ? (this.firstNumber = e.target.innerText)
+              : (this.firstNumber += e.target.innerText);
+
+            this.displayValue = this.firstNumber;
+          } else {
+            this.dotaAdded = true;
+            this.secondNumber == 0
+              ? (this.secondNumber = e.target.innerText)
+              : (this.secondNumber += e.target.innerText);
+            this.displayValue = this.secondNumber;
+          }
+        }
+        this.updateDisplay();
       } else if (e.target.classList.contains("math")) {
         if (this.mathClicked) {
           this.math(this.clickedMathMark);
@@ -49,6 +97,15 @@ const calculator = {
       case "-":
         this.subtract();
         break;
+      case "x":
+        this.multiplication();
+        break;
+      case "/":
+        this.division();
+        break;
+      case "%":
+        this.procent();
+        break;
       default:
         break;
     }
@@ -64,14 +121,29 @@ const calculator = {
 
   addition() {
     this.display.value = this.displayValue =
-      parseInt(this.firstNumber) + parseInt(this.secondNumber);
+      parseFloat(this.firstNumber) + parseFloat(this.secondNumber);
     this.updateDisplay();
   },
 
   subtract() {
     this.display.value = this.displayValue =
-      parseInt(this.firstNumber) - parseInt(this.secondNumber);
+      parseFloat(this.firstNumber) - parseFloat(this.secondNumber);
     this.updateDisplay();
+  },
+
+  multiplication() {
+    this.display.value = this.displayValue =
+      parseFloat(this.firstNumber) * parseFloat(this.secondNumber);
+    this.updateDisplay();
+  },
+  division() {
+    this.display.value = this.displayValue =
+      parseFloat(this.firstNumber) / parseFloat(this.secondNumber);
+    this.updateDisplay();
+  },
+  procent() {
+    this.display.value = this.displayValue =
+      (parseFloat(this.firstNumber) * parseFloat(this.secondNumber)) / 100;
     this.updateDisplay();
   },
 
