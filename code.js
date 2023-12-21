278d7122c7f1fef06fa867015cb30983cf1ab96f
echw <echwedczuk0@gmail.com>
2023-12-21 17:53:01 +0100

    code refactor

--- code.js
+++ code.js
@@ -2,34 +2,46 @@ const calculator = {
   display: document.querySelector("#display"),
   buttons: document.querySelector(".buttons"),
   clear: document.querySelector("#clear"),
-  displayValue: 0,
-  firstNumber: 0,
-  secondNumber: 0,
+  displayValue: "0",
+  firstNumber: "0",
+  secondNumber: "0",
   clickedMathMark: undefined,
-  mathClicked: false,
-  dotaAdded: false,
+  dotAdded: false,
+  dotFirstAdded: false,
+  dotSecondAdded: false,
 
   initEventListeners() {
-    this.buttons.addEventListener("click", (e) => {
-      if (e.target.classList.contains("number")) {
-        if (!this.mathClicked) {
-          this.firstNumber == 0
-            ? (this.firstNumber = e.target.innerText)
-            : (this.firstNumber += e.target.innerText);
+    this.buttons.addEventListener("click", (event) => {
+      this.clickedButton(event.target);
+    });
+  },
+
+  clickedButton(button) {
+    const clicked = button.dataset.button;
+    switch (clicked) {
+      case "number":
+        if (!this.clickedMathMark) {
+          this.firstNumber == "0"
+            ? (this.firstNumber = button.innerText)
+            : (this.firstNumber += button.innerText);
 
           this.displayValue = this.firstNumber;
         } else {
-          this.secondNumber == 0
-            ? (this.secondNumber = e.target.innerText)
-            : (this.secondNumber += e.target.innerText);
+          this.secondNumber == "0"
+            ? (this.secondNumber = button.innerText)
+            : (this.secondNumber += button.innerText);
           this.displayValue = this.secondNumber;
         }
         this.updateDisplay();
-      } else if (e.target.classList.contains("remove")) {
+        break;
+      case "remove":
         if (this.firstNumber.slice(-1) == ".") {
-          this.dotaAdded = false;
+          this.dotFirstAdded = false;
+        } else if (this.secondNumber.slice(-1) == ".") {
+          this.dotSecondAdded = false;
         }
-        if (!this.mathClicked) {
+
+        if (!this.clickedMathMark) {
           this.firstNumber == 0
             ? (this.firstNumber = 0)
             : (this.firstNumber = this.firstNumber.slice(0, -1));
@@ -40,56 +52,57 @@ const calculator = {
             : (this.secondNumber = this.secondNumber.slice(0, -1));
           this.displayValue = this.secondNumber;
         }
+
         if (this.displayValue == "") {
           this.displayValue = 0;
         }
         this.updateDisplay();
-      } else if (e.target.innerText == "=") {
-        this.mathClicked = false;
-        this.math(this.clickedMathMark);
+        break;
+      case "equal":
+        this.calculate(this.clickedMathMark);
         this.firstNumber = this.displayValue;
         this.secondNumber = 0;
-      } else if (e.target.innerText == "C") {
+        break;
+      case "clear":
         this.displayValue = 0;
         this.firstNumber = 0;
         this.secondNumber = 0;
         this.clickedMathMark = undefined;
-        this.mathClicked = false;
-        this.dotaAdded = false;
+        this.dotAdded = false;
         this.updateDisplay();
-      } else if (e.target.innerText == ".") {
-        if (!this.dotaAdded) {
-          if (!this.mathClicked) {
-            this.dotaAdded = true;
-            this.firstNumber == 0
-              ? (this.firstNumber = e.target.innerText)
-              : (this.firstNumber += e.target.innerText);
-
-            this.displayValue = this.firstNumber;
-          } else {
-            this.dotaAdded = true;
-            this.secondNumber == 0
-              ? (this.secondNumber = e.target.innerText)
-              : (this.secondNumber += e.target.innerText);
+        break;
+      case "dot":
+        if (!this.clickedMathMark && !this.dotFirstAdded) {
+          this.dotFirstAdded = true;
+          this.firstNumber += button.innerText;
+          this.displayValue = this.firstNumber;
+        } else {
+          if (!this.dotSecondAdded) {
+            this.dotSecondAdded = true;
+            this.secondNumber += button.innerText;
             this.displayValue = this.secondNumber;
           }
         }
         this.updateDisplay();
-      } else if (e.target.classList.contains("math")) {
-        if (this.mathClicked) {
-          this.math(this.clickedMathMark);
+        break;
+      case "math":
+        if (this.clickedMathMark) {
+          this.calculate(this.clickedMathMark);
           this.firstNumber = this.displayValue;
           this.secondNumber = 0;
+          this.dotFirstAdded = false;
+          this.dotSecondAdded = false;
         }
-        this.mathClicked = true;
-        this.clickedMathMark = e.target.innerText;
+        this.clickedMathMark = button.innerText;
         this.displayValue = 0;
         this.updateDisplay();
-      }
-    });
+        break;
+      default:
+        break;
+    }
   },
 
-  math(operation) {
+  calculate(operation) {
     switch (operation) {
       case "+":
         this.addition();
@@ -104,7 +117,7 @@ const calculator = {
         this.division();
         break;
       case "%":
-        this.procent();
+        this.percent();
         break;
       default:
         break;
@@ -112,37 +125,35 @@ const calculator = {
   },
 
   updateDisplay() {
-    if (!this.mathClicked) {
-      this.display.value = this.displayValue;
-    } else {
-      this.display.value = this.displayValue;
-    }
+    this.display.value = this.displayValue;
   },
 
   addition() {
-    this.display.value = this.displayValue =
+    this.displayValue =
       parseFloat(this.firstNumber) + parseFloat(this.secondNumber);
     this.updateDisplay();
   },
 
   subtract() {
-    this.display.value = this.displayValue =
+    this.displayValue =
       parseFloat(this.firstNumber) - parseFloat(this.secondNumber);
     this.updateDisplay();
   },
 
   multiplication() {
-    this.display.value = this.displayValue =
+    this.displayValue =
       parseFloat(this.firstNumber) * parseFloat(this.secondNumber);
     this.updateDisplay();
   },
+
   division() {
-    this.display.value = this.displayValue =
+    this.displayValue =
       parseFloat(this.firstNumber) / parseFloat(this.secondNumber);
     this.updateDisplay();
   },
-  procent() {
-    this.display.value = this.displayValue =
+
+  percent() {
+    this.displayValue =
       (parseFloat(this.firstNumber) * parseFloat(this.secondNumber)) / 100;
     this.updateDisplay();
   },
